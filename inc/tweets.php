cd262feee9707dc3048a7c4aae779887cf339973
matt jiggins <git@jiggins.ca>
2014-12-29 13:34:23 +0100

    upds

--- inc/tweets.php
+++ inc/tweets.php
@@ -6,7 +6,7 @@ include('keys.php');
 error_reporting( 0 ); // don't let any php errors ruin the feed
 
 $username = 'oliviachow';
-$number_tweets = 20;
+$number_tweets = 25;
 $feed = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name={$username}&count={$number_tweets}&include_rts=1";
 $cache_file = dirname(__FILE__).'/cache/'.'twitter-cache';
 $modified = filemtime( $cache_file );
